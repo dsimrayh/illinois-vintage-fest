9fa1f87f131d0343df34b9e83326bd140154fcb3
Damian <dsimrayh@yahoo.com>
2023-06-15 17:06:42 -0700

    first commit, base files created

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'react/prop-types': 0,
+    quotes: ['warn', 'single', { allowTemplateLiterals: true }],
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import './style.css';
+
+function App() {
+  return <h1>Illinois Vintage Fest</h1>;
+}
+
+export default App;
