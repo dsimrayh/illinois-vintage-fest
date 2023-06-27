fd3a1ba65955f9aa58939c96b86df14f93988656
Damian <dsimrayh@yahoo.com>
2023-06-26 20:32:01 -0700

    added content to header, working on styling and responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import './style.css';
+import './App.css';
 
 function App() {
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,28 @@
+import '../component-stylesheets/Header.css';
+
 export default function Header() {
+  function loadHomePage() {
+    console.log('load home page');
+  }
+
   return (
-    <>
-      <header id="header">header</header>
-    </>
+    <header id="header">
+      <img
+        id="logo-image"
+        src="./src/assets/ivf-logo.png"
+        alt="logo-image"
+        onClick={loadHomePage}
+      ></img>
+      <nav id="nav">
+        <ul id="nav-ul">
+          <li className="nav-item">Upcoming</li>
+          <li className="nav-item">Previous</li>
+          <li className="nav-item">Our Story</li>
+          <li className="nav-item">Shop</li>
+          <li className="nav-item">FAQ</li>
+        </ul>
+      </nav>
+      <button id="header-button">Vintage Fest</button>
+    </header>
   );
 }
